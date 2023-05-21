71461ce8da91b78f1018c83d1cd7ae85adec52e3
Leonid Kurhanskyi <lkurgan55@gmail.com>
2023-05-21 22:48:47 +0300

    add download and read functions

---
+++ src/functions.py
@@ -0,0 +1,34 @@
+import boto3
+import json
+import pandas as pd
+
+def download_data_from_s3(bucket_name, object_key, destination_file_path):
+    """
+    Завантажує об'єкт з Amazon S3 у вказаний шлях файлу.
+    
+    :param bucket_name: Ім'я сховища (bucket) в Amazon S3.
+    :param object_key: Ключ об'єкта (файлу) в Amazon S3.
+    :param destination_file_path: Шлях до файлу, куди буде збережений завантажений об'єкт.
+    """
+    s3_client = boto3.client('s3')
+    
+    try:
+        s3_client.download_file(bucket_name, object_key, destination_file_path)
+        print(f"Файл успішно завантажено з S3. Шлях до файлу: {destination_file_path}")
+    except Exception as e:
+        print(f"Сталася помилка під час завантаження файлу з S3: {e}")
+
+def read_json_file(file_path):
+    """
+    Зчитує JSON-файл за вказаним шляхом та повертає його в форматі словника.
+
+    :param file_path: Шлях до JSON-файлу.
+    :return: Словник, що містить дані з JSON-файлу.
+    """
+    try:
+        with open(file_path, 'r') as json_file:
+            data = json.load(json_file)
+            return data
+    except Exception as e:
+        print(f"Сталася помилка під час читання JSON-файлу: {e}")
+        return None
